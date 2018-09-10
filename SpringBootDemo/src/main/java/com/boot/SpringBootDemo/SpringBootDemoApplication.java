0b69c0f9ddf450d5c0190fc2067f989356b7aee5
shekhar_t <shekharthr@gmail.com>
2018-09-10 17:19:54 +0530

    added Simple output 2

--- SpringBootDemo/src/main/java/com/boot/SpringBootDemo/SpringBootDemoApplication.java
+++ SpringBootDemo/src/main/java/com/boot/SpringBootDemo/SpringBootDemoApplication.java
@@ -9,5 +9,6 @@ public class SpringBootDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDemoApplication.class, args);
 		System.out.println("Test content");
+		System.out.println("Test2 content");
 	}
 }
