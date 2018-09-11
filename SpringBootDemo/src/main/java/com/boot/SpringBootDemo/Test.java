24e34dcc0c46b5b5c4994c5809504641b9ef0033
shekhar_t <shekharthr@gmail.com>
2018-09-11 20:37:32 +0530

    Some changes

--- SpringBootDemo/src/main/java/com/boot/SpringBootDemo/Test.java
+++ SpringBootDemo/src/main/java/com/boot/SpringBootDemo/Test.java
@@ -8,7 +8,7 @@ public class Test {
 	 System.out.println("method call over in master");
 	 System.out.println("method execution completed");
 	 System.out.println("End of the method");
-	 System.out.println("File Data");
+	 System.out.println("File Data changed");
 
  }
 }
